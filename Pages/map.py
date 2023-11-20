e2046d4df59605011d17e8841e8e5f189ad0d59e
Moeez <oct30moeez@gmail.com>
2023-11-20 11:19:06 -0500

    Working On Statsitics Page For Dash

--- Pages/map.py
+++ Pages/map.py
@@ -9,7 +9,7 @@ map_data=Map()
 map_df=map_data.data()
 
 map_figure=px.scatter_mapbox(map_df,lat="Latitude",lon="Longitude",
-                             color="Ridership (2014 Daily Average)",size="Ridership (2014 Daily Average)",
+                             color="Station Name",size="Ridership (2014 Daily Average)",
                              color_continuous_scale=px.colors.cyclical.IceFire,size_max=15,zoom=10,mapbox_style="carto-positron")
 map_figure.update_layout(plot_bgcolor="#000000",paper_bgcolor="#121221",font_color="#FFFFFF")
 
--- Pages/metrics.py
+++ Pages/metrics.py
@@ -2,9 +2,41 @@ import dash
 from dash import Dash,html,dash_table,dcc,Input,Output,callback
 import plotly.express as px
 
+from Analysis.Summary_Statistics import SummaryStatistics
+from Analysis.T_Test import TTest
+
+summarystatistics=SummaryStatistics()
+summarystatistics.busiest_station_2022()
+summarystatistics.quietest_station_2022()
+summarystatistics.busiest_month()
+summarystatistics.quietest_month()
+summarystatistics.busiest_station_per_year()
+
+ttest=TTest()
+ttest.customer_day_vs_hours_per_day()
+ttest.customer_day_vs_km_per_day()
+ttest.customer_day_vs_average_daily_vehicles()
+ttest.graphs()
+
 dash.register_page(__name__)
 
 layout=html.Div([
-    html.H2(children="Metrics Page",style={"textAlign":"center"})
+    html.H2(children="Metrics Page",style={"textAlign":"center"}),
+
+    html.Div(
+        children=[html.H2("2022 Max")],
+        style={"display":"inline-block","width":"25%"},className='divBorder'),
+
+    html.Div(
+        children=[html.H2("2021 Max")],
+        style={"display":"inline-block","width":"25%"},className='divBorder'), 
+
+    html.Div(
+        children=[html.H2("2020 Max")],
+        style={"display":"inline-block","width":"25%"},className='divBorder'),
+
+    html.Div(
+        children=[html.H2("2019 Max")],
+        style={"display":"inline-block","width":"25%"},className='divBorder'),
 
 ])
